8e16fb2a6b3d6704f1156d827b727a229f55636f
mohammadreza <mohammad0reza79@gmail.com>
2022-06-20 17:32:25 +0430

    footer completed but links are not OK

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import classes from "./Fotter.module.css";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className={classes.FooterContainer}>
-      <div className={classes.item}>
+      <div className={classes.item} id={classes.first}>
         <span className={classes.title}>ABOUT</span>
         <span className={classes.AboutText}>
           random-number.org is an online true random generation service. It
@@ -11,13 +12,38 @@ const Footer = () => {
           bits come from a TRNG.
         </span>
       </div>
-      <div className={classes.item}>
+      <div className={classes.item} id={classes.second}>
         <span className={classes.title}>RELATED LINKS</span>
-        <div></div>
+        <div className={classes.relatedLinks}>
+          <Link
+            to={{ pathname: "https://inl-lab.net/" }}
+            style={{ color: "black", marginBottom: "0.75vw" }}
+          >
+            <span>INL Lab</span>
+          </Link>
+          <Link to={{ pathname: "/FAQs" }} style={{ color: "black" }}>
+            <span>FAQs</span>
+          </Link>
+        </div>
       </div>
-      <div className={classes.item}>
+      <div className={classes.item} id={classes.third}>
         <span className={classes.title}>RELATED LINKS</span>
-        <div></div>
+        <div className={classes.relatedLinks}>
+          <Link
+            to={"https://mahdi.jafari.siavoshani.ir/"}
+            target="_blank"
+            style={{ color: "black", marginBottom: "0.75vw" }}
+          >
+            <span>Mahdi Jafari Siavoshani</span>
+          </Link>
+          <Link
+            to={"https://morteza.jafari.siavoshani.ir/"}
+            target="_blank"
+            style={{ color: "black" }}
+          >
+            <span>Morteza Jafari Siavoshani</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
